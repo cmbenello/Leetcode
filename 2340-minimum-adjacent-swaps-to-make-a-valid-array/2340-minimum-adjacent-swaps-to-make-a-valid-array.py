356665c6b3657b068dbe4be53554e68605413de0
Charles Benello <31220609+cmbenello@users.noreply.github.com>
2024-03-31 17:26:38 -0500

    Time: 366 ms (56.32%), Space: 30.8 MB (51.59%) - LeetHub

--- 2340-minimum-adjacent-swaps-to-make-a-valid-array/2340-minimum-adjacent-swaps-to-make-a-valid-array.py
+++ 2340-minimum-adjacent-swaps-to-make-a-valid-array/2340-minimum-adjacent-swaps-to-make-a-valid-array.py
@@ -3,19 +3,27 @@ class Solution:
         #Find the index of the earliest min and max the last max
         # Then just minus the length and then + 1 if they overlap
 
-        max_val = max(nums)
-        min_val = min(nums)
         
-        max_index = nums[::-1].index(max_val)
-        min_index = nums.index(min_val)
+        max_val = -1
+        min_val = float('inf')
         
+        
+        for i in range(len(nums)):
+            if nums[i] >= max_val:
+                max_val = nums[i]
+                max_index = i
+            
+            if nums[i] < min_val:
+                min_val = nums[i]
+                min_index = i
+            
         
         res = 0
         
-        if len(nums) - max_index - 1 < min_index:
+        if max_index < min_index:
             res -= 1
-            
-        res += max_index
+        
+        res += len(nums) - max_index - 1
         res += min_index
         
         return res
